3053efb57a4bbd08e0b2f13ac4a829b8f119a3ef
Dikshya Handique <dikshyah@gmail.com>
2022-08-31 21:10:54 +0530

    class component style

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { Component } from 'react';
+//import { Component } from 'react';
+import { useEffect, useState } from 'react';
 
 import ProductsList from './components/products-list/products-list.component';
 import SearchBox from './components/search-box/search-box.component';
@@ -6,93 +7,133 @@ import SearchBox from './components/search-box/search-box.component';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component{
-  constructor(){
-    super();
-    this.state = {
-      items:[
-      // {
-      //   clothes : 'Printed T-Shirt'
-      // },
-      // {
-      //   clothes : 'Tie-dye T-Shirt'
-      // },
-    ],
-    productsearched : ''
-  };
-  console.log('constructor')
-}
-
-componentDidMount(){
-  console.log('didMount')
-  fetch('https://fakestoreapi.com/products')
-  .then((response) => response.json())
-  .then((products) => this.setState(() =>{
-  //   console.log(products)
-  // }));
-    return {items: products}
-  }),
-  () => {
-    console.log(this.state);
-  }
-  );
-}
-
-searchProductsChange = (event) => {
-  console.log(event.target.value);
-  const productsearched = event.target.value.toLocaleLowerCase() ;
-  this.setState(() => {
-  return {productsearched};
-  });
-}
+const App = () => {
+const [productsearched, productsearchedupdate] = useState('');
+const [items , setItems ] = useState([]);
+const [filteredProducts,setfilteredProducts ] = useState(items);
+console.log({productsearched});
   
-  render() {
-    console.log('render')
-    const { items, productsearched } = this.state;
-    const { searchProductsChange } = this;
-    const filteredProducts = items.filter((item) => {
-      return item.title.toLocaleLowerCase().includes(productsearched);
-    })
+const searchProductsChange = (event) => {
+    console.log(event.target.value);
+    const productsearched_typed = event.target.value.toLocaleLowerCase() ;
+    productsearchedupdate(productsearched_typed);
+  }
 
-    return (
-      <div className="App">
-        <h1 className='app-title'>The Everything Store</h1>
-         <SearchBox onChangeHandler = { searchProductsChange } placeholder = 'search products' className='products-search-box' />       
-        
-        {/* {filteredProducts.map((item) =>{
-          return (
-            <div key={item.id}>  
-              <h1>{item.title}</h1>
-              <button>Add to Cart</button>
-            </div>)   
-        })} */}
+useEffect(() =>{
+  const filteredProducts = items.filter((item) => {
+    return item.title.toLocaleLowerCase().includes(productsearched);
+})
+  setfilteredProducts(filteredProducts);
 
-        <ProductsList productlist = { filteredProducts }/>
+},[items,productsearched]);
 
-
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi {this.state.name}
-          </p>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <button onClick={() => {
-            this.setState({name:'Sam'});
-          }}>Change Name</button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header> */}
-      </div>
+ 
+useEffect(() =>{
+  console.log("effect fired");
+  fetch('https://fakestoreapi.com/products')
+    .then((response) => response.json())
+    .then((products) => setItems(products)
+    //   console.log(products)
+    // })) ;
     );
-  } 
+},[]);
+
+
+  return(
+        <div className="App">
+          <h1 className='app-title'>The Everything Store</h1>
+           <SearchBox onChangeHandler = { searchProductsChange } placeholder = 'search products' className='products-search-box' />       
+           <ProductsList productlist = { filteredProducts }/>
+         </div>)
 }
+
+// class App extends Component{
+//   constructor(){
+//     super();
+//     this.state = {
+//       items:[
+//       // {
+//       //   clothes : 'Printed T-Shirt'
+//       // },
+//       // {
+//       //   clothes : 'Tie-dye T-Shirt'
+//       // },
+//     ],
+//     productsearched : ''
+//   };
+//   console.log('constructor')
+// }
+
+// componentDidMount(){
+//   console.log('didMount')
+//   fetch('https://fakestoreapi.com/products')
+//   .then((response) => response.json())
+//   .then((products) => this.setState(() =>{
+//   //   console.log(products)
+//   // }));
+//     return {items: products}
+//   }),
+//   () => {
+//     console.log(this.state);
+//   }
+//   );
+// }
+
+// searchProductsChange = (event) => {
+//   console.log(event.target.value);
+//   const productsearched = event.target.value.toLocaleLowerCase() ;
+//   this.setState(() => {
+//   return {productsearched};
+//   });
+// }
+  
+//   render() {
+//     console.log('render')
+//     const { items, productsearched } = this.state;
+//     const { searchProductsChange } = this;
+//     const filteredProducts = items.filter((item) => {
+//       return item.title.toLocaleLowerCase().includes(productsearched);
+//     })
+
+//     return (
+//       <div className="App">
+//         <h1 className='app-title'>The Everything Store</h1>
+//          <SearchBox onChangeHandler = { searchProductsChange } placeholder = 'search products' className='products-search-box' />       
+        
+//         {/* {filteredProducts.map((item) =>{
+//           return (
+//             <div key={item.id}>  
+//               <h1>{item.title}</h1>
+//               <button>Add to Cart</button>
+//             </div>)   
+//         })} */}
+
+//         <ProductsList productlist = { filteredProducts }/>
+
+
+//         {/* <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <p>
+//             Hi {this.state.name}
+//           </p>
+//           <p>
+//             Edit <code>src/App.js</code> and save to reload.
+//           </p>
+//           <button onClick={() => {
+//             this.setState({name:'Sam'});
+//           }}>Change Name</button>
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//         </header> */}
+//       </div>
+//     );
+//   } 
+// }
 
 export default App;
--- src/components/product/product-container-component.jsx
+++ src/components/product/product-container-component.jsx
@@ -1,20 +1,14 @@
-import { Component } from "react";
-
 import "./product-container.styles.css";
 
-class ProductContainer extends Component{
-    render(){
-        const { title, description, id, image } = this.props.item;
-        return(
-        <div className='product-container' key={id}>
+const ProductContainer = ({item}) => {
+    const { title, description, id, image } = item
+    return( <div className='product-container' key={id}>
             <img className="product-image" alt ={`product ${title}`} src={image} ></img>
             <h2>{title}</h2>
             <p>{description}</p>
           </div> 
             //  {/* <h1 key = {item.id}>{item.title}</h1> */ }
         )
-    }
-
-}
+    }    
 
 export default ProductContainer;
